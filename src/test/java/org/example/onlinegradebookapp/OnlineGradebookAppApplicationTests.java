51341915d2c088d2f79c01349518b07f0b00a4eb
DamianJ <geniuszmath75@gmail.com>
2025-03-23 21:16:57 +0100

    Create project configuration. Add dependencies and plugins. Connect to database.

---
+++ src/test/java/org/example/onlinegradebookapp/OnlineGradebookAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.onlinegradebookapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineGradebookAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
